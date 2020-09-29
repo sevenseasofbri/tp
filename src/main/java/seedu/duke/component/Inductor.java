b8379e84314d94656b3370976cac057b3c0dbf0f
sevenseasofbri <vishruti2501022gmail.com>
2020-09-30 00:20:38 +0800

    Update Increment A-Inductor

--- src/main/java/seedu/duke/component/Inductor.java
+++ src/main/java/seedu/duke/component/Inductor.java
@@ -1,14 +1,14 @@
 package seedu.duke.component;
 
-public class Inductor extends Component{
-    protected final String UNIT = "µH";
+public class Inductor extends Component {
+    private static final String SI_UNIT = "µH";
 
-    public Inductor(double value){
+    public Inductor(double value) {
         super(value);
     }
 
     @Override
-    public String toString(){
-        return (value)+UNIT;
+    public String toString() {
+        return (value) + SI_UNIT;
     }
 }
