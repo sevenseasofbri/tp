600cde0e601b79308a1c6b390ab19f2c18103a1f
PraveenElango <praveenelango98@gmail.com>
2020-10-01 12:59:06 +0800

    Add Resistor Component

--- src/main/java/seedu/duke/component/Resistor.java
+++ src/main/java/seedu/duke/component/Resistor.java
@@ -6,7 +6,11 @@ public class Resistor extends Component {
     public Resistor(double value){
         super(value);
     }
-    //Returns String with value and SI unit of Resistor
+    /**
+     * Returns the value and unit of Capacitor object.
+     *
+     * @return String of Capacitor value
+     */
     @Override
     public String toString(){
         return (value) + SI_UNIT;
