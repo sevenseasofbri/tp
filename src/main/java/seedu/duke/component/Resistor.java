0cde11c1118660785087a823bfc6fd96fec7e4f0
PraveenElango <praveenelango98@gmail.com>
2020-10-01 12:55:24 +0800

    Included Resistor Component

--- src/main/java/seedu/duke/component/Resistor.java
+++ src/main/java/seedu/duke/component/Resistor.java
@@ -1,7 +1,7 @@
 package seedu.duke.component;
 
 public class Resistor extends Component {
-    private static final String SI_UNIT = "µH";
+    private static final String SI_UNIT = "μH";
 
     public Resistor(double value){
         super(value);
