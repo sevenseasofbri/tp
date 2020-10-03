21c156ece0594f98aed9d2d89c6ac3575f25cc07
sevenseasofbri <vishruti2501022gmail.com>
2020-10-03 13:09:23 +0800

    Remove rl arraylists

--- src/main/java/seedu/duke/template/LrTemplate.java
+++ src/main/java/seedu/duke/template/LrTemplate.java
@@ -4,13 +4,9 @@ import seedu.duke.component.Inductor;
 import seedu.duke.component.Resistor;
 import seedu.duke.component.VoltageSource;
 
-import java.util.ArrayList;
-
 public class LrTemplate extends Template {
-    private Resistor initialResistor;
-    private Inductor initialInductor;
-    private ArrayList<Resistor> resistors;
-    private ArrayList<Inductor> inductors;
+    private Resistor resistor;
+    private Inductor inductor;
     private static final String RL_TEMPLATE = "\t+---R-----L---+\n"
                                             + "\t|             |\n"
                                             + "\t|             |\n"
@@ -18,16 +14,14 @@ public class LrTemplate extends Template {
 
     public LrTemplate(double resistance, double inductance, double powerSupply) {
         super(powerSupply);
-        initialInductor = new Inductor(inductance);
-        inductors.add(initialInductor);
-        initialResistor = new Resistor(resistance);
-        resistors.add(initialResistor);
+        inductor = new Inductor(inductance);
+        resistor = new Resistor(resistance);
     }
 
     @Override
     public String toString() {
-        return RL_TEMPLATE + "Total Resistance: " + initialResistor + System.lineSeparator()
-                + "Total Inductance: " + initialInductor + System.lineSeparator();
+        return RL_TEMPLATE + "Total Resistance: " + resistor + System.lineSeparator()
+                + "Total Inductance: " + inductor + System.lineSeparator();
     }
 
 }
