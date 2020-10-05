bb57f37c200fb1fbb7b22bea238c80229e432076
Dorian <dorianfong@gmail.com>
2020-10-05 19:16:00 +0800

    Add minor changes to VoltageSource

--- src/main/java/seedu/duke/component/VoltageSource.java
+++ src/main/java/seedu/duke/component/VoltageSource.java
@@ -6,12 +6,12 @@ public class VoltageSource extends Component  {
     public VoltageSource(double value) {
         super(value);
     }
+
     /**
      * Returns String constituting value and unit of voltage source of circuit.
      *
      * @return String representation of VoltageSource object.
      */
-
     @Override
     public String toString() {
         return (value) + SI_UNIT;
