2e4656d393237ac5ac886ff9ea2d5f274a5c018e
sevenseasofbri <vishruti250102@gmail.com>
2020-10-19 00:35:04 +0800

    Add JavaDoc to few public methods

--- src/main/java/seedu/duke/component/gates/AndGate.java
+++ src/main/java/seedu/duke/component/gates/AndGate.java
@@ -8,11 +8,21 @@ public class AndGate extends TwoInputGate {
         secondInput = input2;
     }
 
+    /**
+     * Performs bitwise AND (&) operation on the input instance variables.
+     *
+     * @return int value of the gate output.
+     */
     @Override
     public int getOutput() {
         return input & secondInput;
     }
 
+    /**
+     * Returns String representation of the AND gate.
+     *
+     * @return String type depiction of AND gate.
+     */
     @Override
     public String toString() {
         return AND;
