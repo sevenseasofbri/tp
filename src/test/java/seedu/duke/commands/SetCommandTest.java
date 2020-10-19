c833d69cac60ede0ac34f32ac656a60c74ac96ea
PraveenElango <praveenelango98@gmail.com>
2020-10-19 14:27:56 +0800

    Add testing for the remaining commands

---
+++ src/test/java/seedu/duke/commands/SetCommandTest.java
@@ -0,0 +1,29 @@
+package seedu.duke.commands;
+
+import org.junit.jupiter.api.Test;
+import seedu.duke.DukeException;
+import seedu.duke.component.Resistor;
+import seedu.duke.template.RcTemplate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SetCommandTest {
+
+    @Test
+    void execute_voltageSource_setComponent() throws DukeException {
+        RcTemplate t = new RcTemplate(100, 100, 100);
+        SetCommand c = new SetCommand(t, "v", 200);
+        c.execute();
+        assertEquals(c.component, "v");
+        assertEquals(c.componentObject.getValue(), 200);
+    }
+
+    @Test
+    void execute_BadCommand_expectException() {
+        RcTemplate t = new RcTemplate(100, 100, 100);
+        SetCommand c = new SetCommand(t, "v", -100);
+        assertThrows(ArithmeticException.class, c::execute);
+    }
+
+
+}
