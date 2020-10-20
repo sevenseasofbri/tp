f11399debf647b6bb44be3542fa0d794ea9b7278
Wira Azmoon <e0014172@u.nus.edu>
2020-10-20 22:57:54 +0800

    Add default: break; to Parser

--- src/main/java/seedu/duke/logic/parser/Parser.java
+++ src/main/java/seedu/duke/logic/parser/Parser.java
@@ -31,6 +31,8 @@ public class Parser {
             return new ExitCommand();
         case "template":
             return prepareTemplate(args);
+        default:
+            break;
         }
 
         if (isCircuit) {
