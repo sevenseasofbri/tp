e8b1bb751a599a002e4177624e98de8a2f53e2f3
sevenseasofbri <vishruti250102@gmail.com>
2020-10-26 12:04:40 +0800

    Update CalculateCommand to display template after printing output

--- src/main/java/seedu/duke/logic/commands/gates/CalculateBooleanCommand.java
+++ src/main/java/seedu/duke/logic/commands/gates/CalculateBooleanCommand.java
@@ -18,6 +18,6 @@ public class CalculateBooleanCommand extends BooleanCommand {
 
     @Override
     public String toString() {
-        return "The output of the current configuration is " + output + ".";
+        return "The output of the current configuration is " + output + ".\n" + super.toString();
     }
 }
