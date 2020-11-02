4a9fcf3807a01d8bbdbe10a88c9f704539016c45
hughjazzman <e0014172@u.nus.edu>
2020-11-03 00:16:22 +0800

    Update src/main/java/seedu/duke/logic/parser/exceptions/InvalidPositionException.java
    Co-authored-by: sevenseasofbri <57522870+sevenseasofbri@users.noreply.github.com>

--- src/main/java/seedu/duke/logic/parser/exceptions/InvalidPositionException.java
+++ src/main/java/seedu/duke/logic/parser/exceptions/InvalidPositionException.java
@@ -2,6 +2,6 @@ package seedu.duke.logic.parser.exceptions;
 
 public class InvalidPositionException extends ParserException {
     public InvalidPositionException() {
-        super("Invalid position chosen! Make sure the single letter position used is shown within the tree seen.");
+        super("Invalid position chosen! Make sure the single letter position specified is depicted in the current logic diagram.");
     }
 }
