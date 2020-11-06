3bb9c8602c5ca487f53c61b95935ddb29673b540
Dorian <dorianfong@gmail.com>
2020-11-06 20:33:04 +0800

    Update JavaDoc Comments

--- src/main/java/seedu/duke/logic/commands/circuit/CircuitCommand.java
+++ src/main/java/seedu/duke/logic/commands/circuit/CircuitCommand.java
@@ -3,6 +3,9 @@ package seedu.duke.logic.commands.circuit;
 import seedu.duke.logic.commands.Command;
 import seedu.duke.model.template.CircuitTemplate;
 
+/**
+ * The CircuitCommand class inherits from the Command class and is used to create CircuitCommand objects.
+ */
 public abstract class CircuitCommand extends Command {
     protected CircuitTemplate template;
 
