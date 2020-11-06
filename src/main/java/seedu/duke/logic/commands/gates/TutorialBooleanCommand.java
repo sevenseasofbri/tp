0f3d89948542f08cfe53aa5bcdc604ed5e7724fe
PraveenElango <praveenelango98@gmail.com>
2020-11-06 22:39:31 +0800

    Resolve checkstyle violations

--- src/main/java/seedu/duke/logic/commands/gates/TutorialBooleanCommand.java
+++ src/main/java/seedu/duke/logic/commands/gates/TutorialBooleanCommand.java
@@ -13,7 +13,7 @@ public class TutorialBooleanCommand extends SummaryCommand implements TutorialCo
     private static final Parser PARSER = new Parser();
     private int numberOfCommandsDone = 0;
     private static final String[] orderOfInstructions = {"template and", "set B", "set C", "calc",
-            "add C", "set F", "set G", "calc"};
+        "add C", "set F", "set G", "calc"};
     private final Ui ui;
 
     public TutorialBooleanCommand() {
